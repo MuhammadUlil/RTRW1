954f896e3499bc05920b2a911438b8baca0649f5
MuhammadUlil <frostsun15@gmail.com>
2021-05-25 22:46:00 +0700

    RTRW

--- app/Http/Middleware/IsSekretaris.php
+++ app/Http/Middleware/IsSekretaris.php
@@ -3,8 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
-class IsAdmin
+class IsSekretaris
 {
     /**
      * Handle an incoming request.
@@ -13,12 +14,12 @@ class IsAdmin
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()->is_admin == 5) {
+        if (auth()->user()->is_admin == 4) {
             return $next($request);
         }
 
-        return redirect(‘home’)->with(‘error’, "You don't have Bendahara access.");
+        return redirect(‘ / auth / login’)->with(‘error’, "You don't have admin access.");
     }
 }
