c9187a0fc8d099bcfdf4216a1e3368488bca25e0
MuhammadUlil <62757773+MuhammadUlil@users.noreply.github.com>
2021-05-26 15:34:33 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -291,7 +291,60 @@
                     </div>
 
                     @else
-                    <div class=”panel-heading”>Normal User</div>
+                    <div class="body">
+
+                        <div class="row">
+                            <div class="col-md-3">
+                                <a data-toggle="modal" data-target="#surat">
+                                    <div class="card">
+                                        <a href="{{url('/warga')}}">Warga Page</a>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-md-3">
+                                <a data-toggle="modal" data-target="#surat">
+                                    <div class="card">
+                                        <div class="header bg-green">
+                                            <center> AJUKAN SURAT</center>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card">
+                                    <div class="header bg-light-blue">
+                                        <center> STATUS PENGAJUAN SURAT</center>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card">
+                                    <div class="header bg-blue">
+                                        <center> IURAN BULANAN RT</center>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card">
+                                    <div class="header bg-blue">
+                                        <center> PROFIL</center>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card">
+                                    <div class="header bg-blue">
+                                        <center> LAPORAN KEUANGAN</center>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                     @endif
                 </div>
             </div>
